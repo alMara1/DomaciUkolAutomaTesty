7793a7d344e865ef71e956a09d35debb7000e681
alMara <mara.vesely@gmail.com>
2021-11-22 23:30:21 +0100

    Commit na lokalni repository, Ukoly: 1. hotovy, 2. nic 3. rozpracovany

---
+++ src/main/java/cz/czechitas/selenium/TestyPrihlasovaniNaKurzy.java
@@ -0,0 +1,126 @@
+package cz.czechitas.selenium;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TestyPrihlasovaniNaKurzy {
+
+    public static final String UZIVATELSKY_EMAIL = "mara.vesely@gmail.com";
+    public static final String HESLO = "123myJsmeBratri";
+    public static final String URL = "https://cz-test-jedna.herokuapp.com/";
+    public static final String TLACITKO_PRIHLASIT = "//button[@type='submit']";
+    public static final String JMENO_ZAKA = "Albert Jáchym";
+    public static final String PRIJMENI_ZAKA = "Veselý";
+    public static final String DATUM_NAROZENI = "1.11.2011";
+    public static final String TLACITKO_VYTVORIT_PRIHLASKU = "//button[@type='submit']";
+
+    WebDriver prohlizec;
+    WebDriverWait cekani;
+
+
+    @BeforeEach
+    public void setUp() {
+      System.setProperty("webdriver.gecko.driver", System.getProperty("user.home") + "/Java-Training/Selenium/geckodriver");
+//        System.setProperty("webdriver.gecko.driver", "C:\\Java-Training\\Selenium\\geckodriver.exe");
+        prohlizec = new FirefoxDriver();
+      cekani = new WebDriverWait(prohlizec,5);
+        prohlizec.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+    }
+
+    @Disabled
+    @Test // Ukol 1: Test přihlášení existujícího uživatele.
+    public void poZadaniSpravnychUdajuJeUzivatelPrihlasen() {
+
+        prohlizec.navigate().to(URL+"prihlaseni");
+        najdiPodleIdAVlozText("email",UZIVATELSKY_EMAIL);
+        najdiPodleIdAVlozText("password",HESLO);
+        prohlizec.findElement(By.xpath(TLACITKO_PRIHLASIT)).click();
+        Assertions.assertNotNull((cekani.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
+                        ("//*[contains(@class, 'nav-item') and contains(.,'Přihlášen')]")))),
+                "Uživatel není prihlasen");
+    }
+
+    @Test // Ukol 3: Test Přihlásit do systemu -> Vybrat kurz -> Přihlásit dítě
+    public void poPrihlaseniVybraniKurzuaPrihlaseniDiteteJePrihlaskaVSeznamu() {
+        prihlasitUzivateleMarekVesely();
+        najdiPodleXPathAKlikni("//*[contains(@class, 'nav-item') and contains(.,'Pro rodiče')]");
+        najdiPodleXPathAKlikni("//*[contains(@class, 'dropdown-item') and contains(.,'Vytvořit přihlášku')]");
+        najdiPodleXPathAKlikni("button[@class='btn dropdown-toggle btn-light']");
+        najdiPodleXPathAVlozText("//input[@type='search']", "05\n");
+        najdiPodleIdAVlozText("forename", JMENO_ZAKA);
+        najdiPodleIdAVlozText("surname", PRIJMENI_ZAKA);
+        najdiPodleIdAVlozText("birthday", DATUM_NAROZENI);
+        najdiPodleIdAKlikni("payment_fksp");
+        najdiPodleIdAVlozText("note","prihlaska Test 3");
+        najdiPodleIdAKlikni("terms_conditions");
+        prohlizec.findElement(By.xpath(TLACITKO_VYTVORIT_PRIHLASKU)).click();
+
+
+    }
+
+
+
+    public void prihlasitUzivateleMarekVesely() {
+        prohlizec.navigate().to(URL + "prihlaseni");
+        najdiPodleIdAVlozText("email", UZIVATELSKY_EMAIL);
+        najdiPodleIdAVlozText("password", HESLO);
+        prohlizec.findElement(By.xpath(TLACITKO_PRIHLASIT)).click();
+        Assertions.assertNotNull((cekani.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
+                        ("//*[contains(@class, 'nav-item') and contains(.,'Přihlášen')]")))),
+                "Uživatel není prihlasen");
+    }
+
+
+    public void najdiPodleIdAVlozText(String idElementu, String posliText) {
+        cekani.until(ExpectedConditions.visibilityOfElementLocated(By.id(idElementu))).sendKeys(posliText);
+    }
+
+    public void najdiPodleIdAKlikni(String idElementu) {
+        cekani.until(ExpectedConditions.visibilityOfElementLocated(By.id(idElementu))).click();
+    }
+
+    public void najdiPodleXPathAVlozText(String xPathElementu, String posliText) {
+        cekani.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPathElementu))).sendKeys(posliText);
+    }
+
+    public void najdiPodleXPathAKlikni(String xPathElementu) {
+        cekani.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPathElementu))).click();
+    }
+
+    @Disabled
+    @Test
+    public void zviratVTabulceMusiByt7() {
+        prohlizec.navigate().to("https://automation-playground.czechitas.repl.co/");
+        WebElement zalozkaTabulka = prohlizec.findElement(By.id("table"));
+        zalozkaTabulka.click();
+
+        List<WebElement> seznamZvirat = prohlizec.findElements(By.xpath("//table/tbody/tr/td[1]"));
+        Assertions.assertEquals(7, seznamZvirat.size());
+    }
+    @Disabled
+    @Test
+    public void prvniZvireVTabulceMusiBytKocka() {
+        prohlizec.navigate().to("https://automation-playground.czechitas.repl.co/");
+        WebElement zalozkaTabulka = prohlizec.findElement(By.id("table"));
+        zalozkaTabulka.click();
+
+        List<WebElement> seznamZvirat = prohlizec.findElements(By.xpath("//table/tbody/tr/td[1]"));
+        WebElement elementPrvnihoZvirete = seznamZvirat.get(0);
+        String prvniZvire = elementPrvnihoZvirete.getText();
+        Assertions.assertEquals("Kočka", prvniZvire);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        prohlizec.quit();
+    }
+}
